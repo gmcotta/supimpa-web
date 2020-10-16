711207cea52aedf7dfbb83f5768e11597bb7b37e
Gustavo Cotta <gmcotta34@gmail.com>
2020-10-15 23:54:05 -0300

    Getting map coordinates bounds

--- src/pages/MapPage/index.tsx
+++ src/pages/MapPage/index.tsx
@@ -3,6 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import { MdAdd } from 'react-icons/md';
 import { FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LeafletEvent } from 'leaflet';
 import axios from 'axios';
 
 import api from '../../services/api';
@@ -99,8 +100,10 @@ const MapPage: React.FC = () => {
       <MapContainer>
         <Map
           center={cityLocation}
-          zoom={14}
+          zoom={15}
           style={{ width: '100%', height: '100%' }}
+          onmoveend={(event: LeafletEvent) =>
+            console.log(event.target.getBounds())}
         >
           <TileLayer
             url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
